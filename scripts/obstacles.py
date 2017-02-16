d525bc6a70f980ab6f4f488a12c6071f167e9b6e
Jonah Spear <Jonahspear512@gmail.com>
2017-02-16 16:14:35 -0500

    added comments and documentation to obstacles.py

--- scripts/obstacles.py
+++ scripts/obstacles.py
@@ -4,50 +4,97 @@ import math
 
 class Goal(object):
     """Reperesents a something the robot is attracted to"""
-    def __init__(self, x, y, radius, limit, alpha):
+    def __init__(self, x, y, radius, attraction_limit, attraction_factor):
         super(Goal, self).__init__()
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.limit = limit
-        self.alpha = alpha
+        #
+        self.x = x # X position of the goal
+        self.y = y # Y position of the goal
+        self.radius = radius # The radius of the goal (represented as a sphere)
+        self.attraction_limit = attraction_limit # The limit at which the attractive force stops getting larger
+        self.attraction_factor = attraction_factor # Proportional to the strength of the attractive force
+
 
     def get_distance(self, x, y):
+        """
+        Computes the distance between a point and the goal
+
+        Args:
+            x: x position of point we are calculating distance to
+            y: y position of point we are calculating distance to
+        """
         return math.sqrt((self.x - x)**2 + (self.y - y)**2)
 
     def get_mag(self, x, y):
+        """
+        Computes the magnitude of the attractive force at a given point
+
+        Args:
+            x: x position of point we are calculating attractive force for
+            y: y position of point we are calculating attractive force for
+        """
+
+        # Get distance to a point
         d = self.get_distance(x, y)
+        # If within goal, the attractive force is 0
         if d < self.radius:
             return 0
-        if d >= self.radius and (self.limit+self.radius) >= d:
-            return self.alpha * (d - self.radius)
+        # Up to the limit, the attractive force is proportional to d
+        if d >= self.radius and (self.attraction_limit+self.radius) >= d:
+            return self.attraction_factor * (d - self.radius)
+        # Past that, the attractive force is constant
         else:
-            return self.alpha * self.limit
+            return self.attraction_factor * self.attraction_limit
 
     def get_angle(self, x, y):
+        """
+        Computes the angle between the goal and a given point
+
+        Args:
+            x: x position of point we are calculating the angle to
+            y: y position of point we are calculating the angle to
+        """
+
         theta = math.atan2(self.y - y, self.x- x)
         return theta
 
 class Obstacle(object):
     """Represents something the robot is repelled by"""
-    def __init__(self, x, y, radius, limit, beta):
+    def __init__(self, x, y, radius, repellant_limit, repellant_factor):
         super(Obstacle, self).__init__()
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.limit = limit
-        self.beta = beta
+        self.x = x # X position of the obstacle
+        self.y = y # Y position of the obstacle
+        self.radius = radius # The radius of the obstacle (represented as a sphere)
+        self.repellant_limit = repellant_limit # The limit at which the obstacle stops repelling things
+        self.repellant_factor = repellant_factor # Proportional to the strength of the attractive force
 
     def get_mag(self, x, y):
+        """
+        Computes the magnitude of the repellant force at a given point
+
+        Args:
+            x: x position of point we are calculating repellant force for
+            y: y position of point we are calculating repellant force for
+        """
+        # Calculate distance to the point
         d = math.sqrt((self.x - x)**2 + (self.y - y)**2)
+        # If we're inside the obstacle
         if d < self.radius:
-            # if inside of obstacle move really fast out
+            # Move really fast out
             return -5
-        if d >= self.radius and (self.limit+self.radius) >= d:
-             return -self.beta*(self.limit + self.radius - d)
+        # Within the limit the repellant force is proportional to the distance
+        if d >= self.radius and (self.repellant_limit+self.radius) >= d:
+             return -self.repellant_factor*(self.repellant_limit + self.radius - d)
+        # If outside the limit, there is no repellant force
         else:
             return 0
 
     def get_angle(self, x, y):
+        """
+        Computes the angle between the obstacle and a given point
+
+        Args:
+            x: x position of point we are calculating the angle to
+            y: y position of point we are calculating the angle to
+        """
         theta = math.atan2(self.y - y, self.x- x)
         return theta
